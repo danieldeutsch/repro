7c77a87f508f95de217a938bb2d8c9063592651f
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-19 21:19:18 -0400

    Adding setup and version

---
+++ repro/version.py
@@ -0,0 +1,5 @@
+_MAJOR = "0"
+_MINOR = "0"
+_PATCH = "1"
+
+VERSION = f"{_MAJOR}.{_MINOR}.{_PATCH}"
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+# version.py defines the VERSION variable.
+# We use exec here so we don't import repro whilst setting up.
+VERSION = {}
+with open('repro/version.py', 'r') as version_file:
+    exec(version_file.read(), VERSION)
+
+
+setuptools.setup(
+    name='repro',
+    version=VERSION['VERSION'],
+    author='Daniel Deutsch',
+    description='An open-source library for reproducing results from research papers',
+    url='https://github.com/danieldeutsch/repro',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['repro=repro.__main__:main']},
+    python_requires='>=3.6',
+    install_requires=[
+        'black==21.7b0',
+        'datasets==1.9.0',
+        'docker==5.0.0',
+        'overrides==6.1.0',
+        'parameterized==0.8.1',
+        'pytest==6.2.4',
+        'six==1.16.0',
+    ]
+)
