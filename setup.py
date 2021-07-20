a7fb8a7a441f4d3bc92cb90a94d3637f42fab7bc
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-19 21:27:54 -0400

    Adding black to CI workflow

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ import setuptools
 # version.py defines the VERSION variable.
 # We use exec here so we don't import repro whilst setting up.
 VERSION = {}
-with open('repro/version.py', 'r') as version_file:
+with open("repro/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
 
 
 setuptools.setup(
-    name='repro',
-    version=VERSION['VERSION'],
-    author='Daniel Deutsch',
-    description='An open-source library for reproducing results from research papers',
-    url='https://github.com/danieldeutsch/repro',
+    name="repro",
+    version=VERSION["VERSION"],
+    author="Daniel Deutsch",
+    description="An open-source library for reproducing results from research papers",
+    url="https://github.com/danieldeutsch/repro",
     packages=setuptools.find_packages(),
-    entry_points={'console_scripts': ['repro=repro.__main__:main']},
-    python_requires='>=3.6',
+    entry_points={"console_scripts": ["repro=repro.__main__:main"]},
+    python_requires=">=3.6",
     install_requires=[
-        'pytest==6.2.4',
-    ]
+        'black==21.7b0',
+        "pytest==6.2.4",
+    ],
 )
