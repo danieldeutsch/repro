2a60c3671173cb3066f75a4b40e0c2191895e9e5
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-20 19:54:29 -0400

    Adding environment variable to control GPU devices for testing

--- models/gupta2020/tests/model_test.py
+++ models/gupta2020/tests/model_test.py
@@ -3,6 +3,7 @@ import unittest
 from parameterized import parameterized
 
 from repro.models.gupta2020 import Gupta2020
+from repro.testing import get_testing_device_parameters
 
 from . import FIXTURES_ROOT
 
@@ -12,7 +13,7 @@ class TestGupta2020(unittest.TestCase):
         # The examples were taken from the AllenNLP demo for this model
         self.expected_outputs = json.load(open(f"{FIXTURES_ROOT}/expected-output.json"))
 
-    @parameterized.expand([(-1,), (0,)])
+    @parameterized.expand(get_testing_device_parameters())
     def test_gupta2020(self, device: int):
         model = Gupta2020(device=device)
         predictions = model.predict_batch(self.expected_outputs)
