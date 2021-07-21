67c563ec8b23e36918bb7eb53848cc68af641254
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-21 12:44:16 -0400

    Updating

--- models/lewis2020/experiments/reproduce-results/run.sh
+++ models/lewis2020/experiments/reproduce-results/run.sh
@@ -1,16 +1,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -e
-
-#repro predict \
-#  --model-name lewis2020-bart \
-#  --dataset-name cnn_dailymail \
-#  --split test \
-#  --output-writer sacrerouge \
-#  --output-file ${DIR}/output/cnn_dailymail/predictions.jsonl
+DEVICE=1
 
 repro predict \
   --model-name lewis2020-bart \
-  --model-args '{"pretrained_model": "bart.large.xsum"}' \
+  --model-args '{"device": '${DEVICE}'}' \
+  --dataset-name cnn_dailymail \
+  --split test \
+  --output-writer sacrerouge \
+  --output-file ${DIR}/output/cnn_dailymail/predictions.jsonl
+
+repro predict \
+  --model-name lewis2020-bart \
+  --model-args '{"device": '${DEVICE}', "pretrained_model": "bart.large.xsum"}' \
   --dataset-name xsum \
   --split test \
   --output-writer sacrerouge \
