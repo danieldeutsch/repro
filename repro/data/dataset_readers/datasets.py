5b3f48cc2d9efd20ca468b3fd738a3f0af7ae9c9
Daniel Deutsch <danieldeutsch@users.noreply.github.com>
2021-07-21 20:45:33 -0400

    Adding HF dataset reader tests (#22)

--- repro/data/dataset_readers/datasets.py
+++ repro/data/dataset_readers/datasets.py
@@ -1,4 +1,6 @@
 import datasets
+import os
+from datasets.config import HF_DATASETS_CACHE
 from overrides import overrides
 from typing import List
 
@@ -6,6 +8,31 @@ from repro.data.dataset_readers import DatasetReader
 from repro.data.types import InstanceDict
 
 DEFAULT_VERSIONS = {"cnn_dailymail": "3.0.0", "xsum": "1.2.0"}
+
+
+def hf_dataset_exists_locally(name: str, version: str = None) -> bool:
+    """
+    Checks to see if a Huggingface `datasets` dataset exists locally in the cache.
+    The logic checks to see if the directory exists where the data should be, but
+    does not do any further verification.
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset, like "cnn_dailymail"
+    version : str, default=None
+        The version of the dataset, like "3.0.0". If `None`, then the
+        default version is used.
+
+    Returns
+    -------
+    bool
+        True if the dataset exists, False otherwise.
+    """
+    if version is None:
+        version = DEFAULT_VERSIONS[name]
+    data_dir = os.path.join(HF_DATASETS_CACHE, name, version)
+    return os.path.exists(data_dir)
 
 
 @DatasetReader.register("huggingface-datasets")
@@ -31,12 +58,11 @@ class HuggingfaceDatasetsDatasetReader(DatasetReader):
                 f"Unknown dataset format. Expected 0 or 1 '/': {self.dataset_name}"
             )
 
-        dataset_splits = datasets.load_dataset(name, version)
-        split = dataset_splits[self.split]
+        dataset = datasets.load_dataset(name, version, split=self.split)
 
         instances = []
         if name == "cnn_dailymail":
-            for instance in split:
+            for instance in dataset:
                 # The reference sentences are separated by \n, but the document
                 # is not sentence split already
                 reference = instance["highlights"].split("\n")
@@ -48,7 +74,7 @@ class HuggingfaceDatasetsDatasetReader(DatasetReader):
                     }
                 )
         elif name == "xsum":
-            for instance in split:
+            for instance in dataset:
                 # The documents include \n characters to separate sentences. The
                 # summaries are only one sentence, so it does not matter for them
                 document = instance["document"].split("\n")
@@ -60,9 +86,9 @@ class HuggingfaceDatasetsDatasetReader(DatasetReader):
                     }
                 )
         elif name == "scientific_papers":
-            for i, instance in enumerate(split):
+            for i, instance in enumerate(dataset):
                 # There is no instance_id in `datasets`, so we make one up
-                instance_id = f"{version}-{split}-{i}"
+                instance_id = f"{version}-{self.split}-{i}"
 
                 # The articles and abstracts have paragraphs split by \n
                 document = instance["article"].split("\n")
---
+++ tests/data/dataset_readers/datasets_test.py
@@ -0,0 +1,60 @@
+import json
+import pytest
+import unittest
+
+from repro.data.dataset_readers import HuggingfaceDatasetsDatasetReader
+from repro.data.dataset_readers.datasets import hf_dataset_exists_locally
+from tests import FIXTURES_ROOT
+
+
+class TestHuggingfaceDatasetsDatasetReader(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expected_output = json.load(
+            open(f"{FIXTURES_ROOT}/hf-datasets/expected-output.json", "r")
+        )
+
+    @pytest.mark.skipif(
+        not hf_dataset_exists_locally("cnn_dailymail"),
+        reason="CNN/DailyMail dataset does not exist",
+    )
+    def test_cnn_dailymail(self):
+        reader = HuggingfaceDatasetsDatasetReader("cnn_dailymail", "test")
+        instances = reader.read()
+        assert len(instances) == 11490
+
+        expected_instances = self.expected_output["cnn_dailymail"]
+        for actual, expected in zip(instances, expected_instances):
+            assert actual == expected
+
+    @pytest.mark.skipif(
+        not hf_dataset_exists_locally("xsum"),
+        reason="XSum dataset does not exist",
+    )
+    def test_xsum(self):
+        reader = HuggingfaceDatasetsDatasetReader("xsum", "test")
+        instances = reader.read()
+        assert len(instances) == 11334
+
+        expected_instances = self.expected_output["xsum"]
+        for actual, expected in zip(instances, expected_instances):
+            assert actual == expected
+
+    @pytest.mark.skipif(
+        not hf_dataset_exists_locally("scientific_papers", "arxiv"),
+        reason="scientific_papers/arxiv dataset does not exist",
+    )
+    def test_arxiv(self):
+        # We don't check for the parsing correctness because the documents are too long
+        reader = HuggingfaceDatasetsDatasetReader("scientific_papers/arxiv", "test")
+        instances = reader.read()
+        assert len(instances) == 6440
+
+    @pytest.mark.skipif(
+        not hf_dataset_exists_locally("scientific_papers", "pubmed"),
+        reason="scientific_papers/pubmed dataset does not exist",
+    )
+    def test_pubmed(self):
+        # We don't check for the parsing correctness because the documents are too long
+        reader = HuggingfaceDatasetsDatasetReader("scientific_papers/pubmed", "test")
+        instances = reader.read()
+        assert len(instances) == 6658
