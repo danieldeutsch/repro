2e710abdac97ea65a48503511a2e53738c24e267
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-23 15:06:26 -0400

    Removing the docker build arg

--- models/dou2021/scripts/download-models.sh
+++ models/dou2021/scripts/download-models.sh
@@ -1,11 +1,7 @@
-if [ "$SENTENCE_GUIDED" = "true" ]; then
-  mkdir bart_sentence
-  cd bart_sentence
-  echo "HERE"
-  gdown https://drive.google.com/uc?id=1BMKhAh2tG5p8THxugZWMPc7NXqwJDHLw
-  mv bart_sentence.pt model.pt
-  wget https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt
-  mv dict.txt dict.source.txt
-  cp dict.source.txt dict.target.txt
-  cd ..
-fi
+mkdir bart_sentence
+cd bart_sentence
+gdown https://drive.google.com/uc?id=1BMKhAh2tG5p8THxugZWMPc7NXqwJDHLw
+mv bart_sentence.pt model.pt
+wget https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt
+mv dict.txt dict.source.txt
+cp dict.source.txt dict.target.txt
--- models/dou2021/src/setup.py
+++ models/dou2021/src/setup.py
@@ -1,38 +1,9 @@
-import argparse
-
-from overrides import overrides
-
 from repro import MODELS_ROOT
 from repro.commands.subcommand import SetupSubcommand
-from repro.common.docker import BuildDockerImageSubcommand, build_image
+from repro.common.docker import BuildDockerImageSubcommand
 
 
 @SetupSubcommand.register("dou2021")
 class Dou2021SetupSubcommand(BuildDockerImageSubcommand):
     def __init__(self) -> None:
         super().__init__("dou2021", f"{MODELS_ROOT}/dou2021")
-
-    @overrides
-    def add_subparser(self, parser: argparse._SubParsersAction):
-        description = f'Build the docker image "{self.image}"'
-        self.parser = parser.add_parser(
-            self.image, description=description, help=description
-        )
-        self.parser.add_argument(
-            "--sentence-guided",
-            action="store_true",
-            help="Indicates the sentence-guided model should be downloaded",
-        )
-        self.parser.add_argument(
-            "--silent",
-            action="store_true",
-            help="Silences the output from the build command",
-        )
-        self.parser.set_defaults(subfunc=self.run)
-
-    @overrides
-    def run(self, args):
-        build_args = {
-            "SENTENCE_GUIDED": "true" if args.sentence_guided else "false",
-        }
-        build_image(self.root, self.image, build_args=build_args, silent=args.silent)
