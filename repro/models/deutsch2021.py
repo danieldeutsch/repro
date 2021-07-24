a42e40882efc72571d2a17c3d702e645b12ffa3d
Daniel Deutsch <danieldeutsch@users.noreply.github.com>
2021-07-24 19:58:42 -0400

    Adding QAEval models (#30)

---
+++ repro/models/deutsch2021.py
@@ -0,0 +1,5 @@
+from ._models.deutsch2021.src.models import (
+    QAEvalQuestionAnsweringModel,
+    QAEvalQuestionGenerationModel,
+)
+from ._models.deutsch2021.src.setup import Deutsch2021SetupSubcommand
