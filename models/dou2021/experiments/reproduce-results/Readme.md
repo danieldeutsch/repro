06789eb90b7468e915818a864684b10e08d50896
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-25 15:50:00 -0400

    Intermediate commit

--- models/dou2021/experiments/reproduce-results/Readme.md
+++ models/dou2021/experiments/reproduce-results/Readme.md
@@ -2,23 +2,9 @@
 This experiment runs the oracle sentence-guided model and evaluates its predictions with ROUGE to compare to what is reported in the paper.
 
 ## Usage
-```
-sh models/dou2021/experiments/reproduce-results/run.sh
-```
-
-After prediction is done, we calculated the ROUGE scores via SacreROUGE.
-This code is not included in the script because SacreROUGE is not a dependency right now.
-It is necessary to sentence-split the CNN/DailyMail predictions in order to reproduce the ROUGE-L score.
-We did this with [`sentence_split.py`](sentence_split.py) and `spacy==2.3.3` and `en_core_web_sm==2.3.1`.
+Running this experiments requires having ROUGE setup (see [here](../../../sacrerouge/Readme.md)).
 ```shell script
-python sentence_split.py --input-file predictions.jsonl --output-file predictions-split.jsonl
-
-sacrerouge rouge evaluate \
-    --input-files <path/to/predictions-tokenized.jsonl> \
-    --macro-output-json macro.json \
-    --micro-output-jsonl micro.jsonl \
-    --dataset-reader reference-based \
-    --compute_rouge_l true
+sh models/dou2021/experiments/reproduce-results/run.sh
 ```
 
 ## Results
--- models/dou2021/experiments/reproduce-results/run.sh
+++ models/dou2021/experiments/reproduce-results/run.sh
@@ -2,35 +2,67 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -e
 DEVICE=1
 
-if [[ ! -d ${DIR}/output/cnn_dm ]]; then
+if [[ ! -d ${DIR}/output/data/input ]]; then
   gdown https://drive.google.com/uc?id=1goxgX-0_2Jo7cNAFrsb9BJTsrH7re6by
   tar -xvf cnn_dm.tar.gz
   rm cnn_dm.tar.gz
-  mv cnn_dm ${DIR}/output
+  mkdir -p ${DIR}/output/data
+  mv cnn_dm ${DIR}/output/data/input
 fi
 
-if [[ ! -d ${DIR}/output/oracle ]]; then
+if [[ ! -d ${DIR}/output/data/oracle ]]; then
   gdown https://drive.google.com/uc?id=12SpWwfD3syIxcC-SdSNnDOI5sbXJaylC
   unzip bart_sentence_guide_cnndm.zip
   rm bart_sentence_guide_cnndm.zip
-  mkdir -p ${DIR}/output/oracle
-  mv train.oracle val.oracle test.oracle test.matchsum ${DIR}/output/oracle
+  mkdir -p ${DIR}/output/data/oracle
+  mv train.oracle val.oracle test.oracle test.matchsum ${DIR}/output/data/oracle
 fi
 
-for model in "dou2021-oracle-sentence-gsum"; do
+for model in "dou2021-oracle-sentence-gsum" "dou2021-sentence-gsum"; do
+#  # Run on the standard version of the dataset
 #  repro predict \
 #    --model-name ${model} \
 #    --model-args '{"device": '${DEVICE}'}' \
 #    --dataset-name cnn_dailymail/3.0.0 \
 #    --split test \
 #    --output-writer sacrerouge \
-#    --output-file ${DIR}/output/cnn_dailymail/${model}/predictions.jsonl
-#
-  repro predict \
-    --model-name ${model} \
-    --model-args '{"device": '${DEVICE}'}' \
-    --dataset-reader dou2021 \
-    --input-files ${DIR}/output/cnn_dm/test.source ${DIR}/output/cnn_dm/test.target ${DIR}/output/oracle/test.oracle \
-    --output-writer sacrerouge \
-    --output-file ${DIR}/output/cnn_dailymail-preproc/${model}/predictions.jsonl
+#    --output-file ${DIR}/output/original/${model}/predictions.jsonl
+
+#  # Run on their preprocessed version
+#  repro predict \
+#    --model-name ${model} \
+#    --model-args '{"device": '${DEVICE}'}' \
+#    --dataset-reader dou2021 \
+#    --input-files ${DIR}/output/data/input/test.source ${DIR}/output/data/input/test.target \
+#    --output-writer sacrerouge \
+#    --output-file ${DIR}/output/preprocessed/${model}/predictions.jsonl
+
+  if [ "${model}" = "dou2021-oracle-sentence-gsum" ]; then
+    # Run with their oracle guidance
+    repro predict \
+      --model-name ${model} \
+      --model-args '{"device": '${DEVICE}'}' \
+      --dataset-reader dou2021 \
+      --input-files ${DIR}/output/data/input/test.source ${DIR}/output/data/input/test.target ${DIR}/output/data/oracle/test.oracle \
+      --output-writer sacrerouge \
+      --output-file ${DIR}/output/oracle/${model}/predictions.jsonl
+
+    # Run with their MatchSum guidance
+    repro predict \
+      --model-name ${model} \
+      --model-args '{"device": '${DEVICE}'}' \
+      --dataset-reader dou2021 \
+      --input-files ${DIR}/output/data/input/test.source ${DIR}/output/data/input/test.target ${DIR}/output/data/oracle/test.matchsum \
+      --output-writer sacrerouge \
+      --output-file ${DIR}/output/matchsum/${model}/predictions.jsonl
+  fi
+
+  # Evaluate with ROUGE
+  for version in "original" "preprocessed" "oracle" "matchsum"; do
+    if [ -f ${DIR}/output/${version}/${model}/predictions.jsonl ]; then
+      python models/sacrerouge/scripts/calculate_rouge.py \
+        --input-file ${DIR}/output/${version}/${model}/predictions.jsonl \
+        --output-file ${DIR}/output/${version}/${model}/rouge.json
+    fi
+  done
 done
