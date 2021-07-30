79d8223496030c0123d1eacbbee9f7882b8842b6
Daniel Deutsch <danfdeutsch@gmail.com>
2021-07-30 10:19:40 -0400

    Adding util functions for grouping by references

--- repro/common/util.py
+++ repro/common/util.py
@@ -1,4 +1,6 @@
-from typing import List, Union
+from typing import List, T, Tuple, Union
+
+from repro.data.types import TextType
 
 
 def flatten(text: Union[str, List[str]], separator: str = None) -> str:
@@ -23,3 +25,88 @@ def flatten(text: Union[str, List[str]], separator: str = None) -> str:
     if isinstance(text, list):
         return separator.join(text)
     return text
+
+
+def _hash_texts(texts: List[TextType]) -> int:
+    hashes = []
+    for text in texts:
+        if isinstance(text, str):
+            hashes.append(hash(text))
+        else:
+            hashes.append(hash(tuple(text)))
+    return hash(tuple(hashes))
+
+
+def group_by_references(
+    candidates: List[TextType], references_list: List[List[TextType]]
+) -> Tuple[List[List[TextType]], List[List[TextType]], List[Tuple[int, int]]]:
+    """
+    Groups the `candidates` by identical references in `references_list`. This is
+    enables evaluation metrics which need to do a lot of preprocessing of the
+    references to reducing the amount of duplicate effort required.
+
+    Parameters
+    ----------
+    candidates : List[TextType]
+        The candidate texts
+    references_list : List[List[TextType]]
+        The reference texts.
+
+    Returns
+    -------
+    List[List[TextType]]
+        The grouped candidate texts in which the candidates at index `i` all
+        share the same references in the grouped references
+    List[List[TextType]]
+        The grouped reference texts
+    List[Tuple[int, int]]
+        A mapping from the input `candidates` to the `(i, j)` position
+        in the grouped candidates that it corresponds to.
+    """
+    mapping = []
+    references_to_index = {}
+    grouped_candidates_list = []
+    grouped_references_list = []
+
+    for candidate, references in zip(candidates, references_list):
+        references_key = _hash_texts(references)
+        if references_key not in references_to_index:
+            # This is a new set of references. Update the data structures
+            group_index = len(grouped_references_list)
+            references_to_index[references_key] = group_index
+            grouped_candidates_list.append([])
+            grouped_references_list.append(references)
+
+        group_index = references_to_index[references_key]
+        candidate_index = len(grouped_candidates_list[group_index])
+        grouped_candidates_list[group_index].append(candidate)
+        mapping.append((group_index, candidate_index))
+
+    return grouped_candidates_list, grouped_references_list, mapping
+
+
+def ungroup_values(
+    values_list: List[List[T]], mapping: List[Tuple[int, int]]
+) -> List[T]:
+    """
+    Ungroups the values in `values_list` based on the group `mapping`. `mapping[i]` specifies
+    the `(j, k)` position in `values_list` that the value for `i` corresponds to. The
+    result of the function is the ungrouped values for each item in `mapping`.
+
+    Parameters
+    ----------
+    values_list : List[List[T]]
+        The grouped values
+    mapping : List[Tuple[int, int]]
+        The mapping which specifies where in the grouped values each
+        item in the original order is in.
+
+    Returns
+    -------
+    List[T]
+        The ungrouped values.
+    """
+    ungrouped_values = []
+    for i, j in mapping:
+        ungrouped_values.append(values_list[i][j])
+    return ungrouped_values
--- tests/common/util_test.py
+++ tests/common/util_test.py
@@ -9,3 +9,139 @@ class TestUtil(unittest.TestCase):
         assert util.flatten("text", separator="-") == "text"
         assert util.flatten(["1", "2"]) == "1 2"
         assert util.flatten(["1", "2"], separator="-") == "1-2"
+
+    def test_group_by_references(self):
+        # Each `references` can either be
+        #   (1) a List[str] of length 1 for a single reference
+        #   (2) a List[List[str]] of length 1 for a single reference
+        #   (3) a List[str] of length >1 for multiple references
+        #   (4) a List[List[str]] of length >2 for multiple references
+        # This method tests to ensure duplicates of each type are correctly
+        # identified.
+        candidates = ["1", "2", "3", "4"]
+
+        # All unique
+        references_list = [
+            ["A"],
+            [["B1", "B2"]],
+            ["C", "D"],
+            ["E", ["F1", "F2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1"], ["2"], ["3"], ["4"]]
+        assert grouped_references_list == references_list
+        assert mapping == [(0, 0), (1, 0), (2, 0), (3, 0)]
+
+        # Duplicates which are single-reference strings
+        references_list = [
+            ["A"],
+            [["B1", "B2"]],
+            ["A"],
+            ["E", ["F1", "F2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1", "3"], ["2"], ["4"]]
+        assert grouped_references_list == [["A"], [["B1", "B2"]], ["E", ["F1", "F2"]]]
+        assert mapping == [(0, 0), (1, 0), (0, 1), (2, 0)]
+
+        # Duplicates which are single-reference List[str]. Index 2
+        # should not be the same as 1 and 3 because it is 2 references, not 1.
+        references_list = [
+            ["A"],
+            [["B1", "B2"]],
+            ["B1", "B2"],
+            [["B1", "B2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1"], ["2", "4"], ["3"]]
+        assert grouped_references_list == [["A"], [["B1", "B2"]], ["B1", "B2"]]
+        assert mapping == [(0, 0), (1, 0), (2, 0), (1, 1)]
+
+        # Duplicates which are multi-ref, each reference is a str
+        references_list = [
+            ["A", "B"],
+            [["C1", "C2"]],
+            ["A", "B"],
+            ["D", ["E1", "E2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1", "3"], ["2"], ["4"]]
+        assert grouped_references_list == [
+            ["A", "B"],
+            [["C1", "C2"]],
+            ["D", ["E1", "E2"]],
+        ]
+        assert mapping == [(0, 0), (1, 0), (0, 1), (2, 0)]
+
+        # Duplicates which are multi-ref, each reference is a List[str]
+        references_list = [
+            ["A"],
+            [["B1", "B2"], ["C1"]],
+            [["B1", "B2"], ["C1"]],
+            ["D", ["E1", "E2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1"], ["2", "3"], ["4"]]
+        assert grouped_references_list == [
+            ["A"],
+            [["B1", "B2"], ["C1"]],
+            ["D", ["E1", "E2"]],
+        ]
+        assert mapping == [(0, 0), (1, 0), (1, 1), (2, 0)]
+
+        # Duplicates which are multi-ref, each reference is a different type
+        references_list = [
+            ["A"],
+            ["B", ["C1", "C2"]],
+            [["D1", "D2"], ["D1"]],
+            ["B", ["C1", "C2"]],
+        ]
+        (
+            grouped_candidates_list,
+            grouped_references_list,
+            mapping,
+        ) = util.group_by_references(candidates, references_list)
+        assert grouped_candidates_list == [["1"], ["2", "4"], ["3"]]
+        assert grouped_references_list == [
+            ["A"],
+            ["B", ["C1", "C2"]],
+            [["D1", "D2"], ["D1"]],
+        ]
+        assert mapping == [(0, 0), (1, 0), (2, 0), (1, 1)]
+
+    def test_ungroup_values(self):
+        values_list = [
+            [1, 2],
+            [3],
+            [4, 5]
+        ]
+        mapping = [
+            (0, 0), (0, 1), (1, 0), (2, 0), (2, 1)
+        ]
+        assert util.ungroup_values(values_list, mapping) == [1, 2, 3, 4, 5]
+
+        mapping = [
+            (1, 0), (0, 0), (0, 1), (2, 1), (2, 0)
+        ]
+        assert util.ungroup_values(values_list, mapping) == [3, 1, 2, 5, 4]
+        
