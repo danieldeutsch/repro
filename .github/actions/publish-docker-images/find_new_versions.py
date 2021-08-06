54f9ebfcdbb8d31200378d6683cc84e145793cce
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-06 13:48:42 -0400

    Updating

---
+++ .github/actions/publish-docker-images/find_new_versions.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--master-sha", required=True)
+    argp.add_argument("--current-sha", required=True)
+    args = argp.parse_args()
+    main(args)
---
+++ models/deutsch2021/src/metadata.py
@@ -0,0 +1,13 @@
+VERSION = "1.0"
+MODEL_NAME = "deutsch2021"
+DEFAULT_IMAGE = f"danieldeutsch/{MODEL_NAME}:{VERSION}"
+CITATION = """
+@misc{deutsch2021questionanswering,
+      title={{Towards Question-Answering as an Automatic Metric for Evaluating the Content Quality of a Summary}}, 
+      author={Daniel Deutsch and Tania Bedrax-Weiss and Dan Roth},
+      year={2021},
+      eprint={2010.00490},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+"""
