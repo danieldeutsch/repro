259cfc9c7cf3516104af768359e9e512b076e092
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-06 15:21:45 -0400

    Updating

--- models/lin2004/src/setup.py
+++ models/lin2004/src/setup.py
@@ -5,7 +5,7 @@ from repro.common.docker import BuildDockerImageSubcommand
 from .metadata import MODEL_NAME, DEFAULT_IMAGE
 
 
-@SetupSubcommand.register("lin2004")
+@SetupSubcommand.register(MODEL_NAME)
 class Lin2004SetupSubcommand(BuildDockerImageSubcommand):
     def __init__(self) -> None:
         super().__init__(MODEL_NAME, DEFAULT_IMAGE, f"{MODELS_ROOT}/{MODEL_NAME}")
