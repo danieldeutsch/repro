6cf286913593a7e7ee916736b4f30a45e28bc22c
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-06 15:08:49 -0400

    Updating

--- models/deutsch2021/src/metadata.py
+++ models/deutsch2021/src/metadata.py
@@ -2,13 +2,3 @@ VERSION = "1.0"
 MODEL_NAME = "deutsch2021"
 DOCKERHUB_REPRO = f"danieldeutsch/{MODEL_NAME}"
 DEFAULT_IMAGE = f"{DOCKERHUB_REPRO}:{VERSION}"
-CITATION = """
-@misc{deutsch2021questionanswering,
-      title={{Towards Question-Answering as an Automatic Metric for Evaluating the Content Quality of a Summary}}, 
-      author={Daniel Deutsch and Tania Bedrax-Weiss and Dan Roth},
-      year={2021},
-      eprint={2010.00490},
-      archivePrefix={arXiv},
-      primaryClass={cs.CL}
-}
-"""
