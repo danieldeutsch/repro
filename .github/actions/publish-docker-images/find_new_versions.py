a8c5c1ef7b653d3f93374c4608bce69151ce7a73
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-06 13:51:00 -0400

    Updating

--- .github/actions/publish-docker-images/find_new_versions.py
+++ .github/actions/publish-docker-images/find_new_versions.py
@@ -5,7 +5,7 @@ def main(args):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     argp = argparse.ArgumentParser()
     argp.add_argument("--master-sha", required=True)
     argp.add_argument("--current-sha", required=True)
