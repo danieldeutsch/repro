9201825b0e1728b35e75eee8149a4f3b60642152
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-08 19:07:48 -0400

    Fixing

--- repro/models/gupta2020/__init__.py
+++ repro/models/gupta2020/__init__.py
@@ -1,3 +1,3 @@
-from repro.models.gupta2020.src.metadata import *
+from repro.models.gupta2020.metadata import *
 from repro.models.gupta2020.model import NeuralModuleNetwork
 from repro.models.gupta2020.setup import Gupta2020SetupSubcommand
--- repro/models/kryscinski2019/__init__.py
+++ repro/models/kryscinski2019/__init__.py
@@ -1,3 +1,3 @@
-from repro.models.kryscinski2019.src.metadata import *
+from repro.models.kryscinski2019.metadata import *
 from repro.models.kryscinski2019.models import FactCC, FactCCX
 from repro.models.kryscinski2019.setup import Kryscinski2019SetupSubcommand
