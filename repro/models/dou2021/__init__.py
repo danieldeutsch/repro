765eeca836dd6781d390d5577683c268c8a979b0
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-08 18:49:07 -0400

    Moving dou2021

---
+++ repro/models/dou2021/__init__.py
@@ -0,0 +1,4 @@
+from repro.models.dou2021.dataset_reader import Dou2021DatasetReader
+from repro.models.dou2021.metadata import *
+from repro.models.dou2021.models import OracleSentenceGSumModel, SentenceGSumModel
+from repro.models.dou2021.setup import Dou2021SetupSubcommand
