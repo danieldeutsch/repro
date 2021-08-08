8630a0cfa23577a502d27a013f6a301adb5148ef
Daniel Deutsch <danfdeutsch@gmail.com>
2021-08-08 18:47:44 -0400

    Moving Deutsch 2021

---
+++ repro/models/deutsch2021/__init__.py
@@ -0,0 +1,10 @@
+from repro.models.deutsch2021.dataset_reader import (
+    Deutsch2021QuestionAnsweringEvaluationDatasetReader,
+)
+from repro.models.deutsch2021.metadata import *
+from repro.models.deutsch2021.models import (
+    QAEval,
+    QAEvalQuestionAnsweringModel,
+    QAEvalQuestionGenerationModel,
+)
+from repro.models.deutsch2021.setup import Deutsch2021SetupSubcommand
