e82a6d9f6563b943523af31c7b3a7484f795bf8f
Daniel Deutsch <danfdeutsch@gmail.com>
2021-10-02 12:57:29 -0400

    Intermediate commit

---
+++ models/yuan2021/tests/model_test.py
@@ -0,0 +1,29 @@
+import json
+import pytest
+import unittest
+from parameterized import parameterized
+
+from repro.models.yuan2021 import BARTScore
+from repro.testing import FIXTURES_ROOT as REPRO_FIXTURES_ROOT
+from repro.testing import assert_dicts_approx_equal, get_testing_device_parameters
+
+from . import FIXTURES_ROOT
+
+
+class TestYuan2021Model(unittest.TestCase):
+    def setUp(self) -> None:
+        self.examples = json.load(
+            open(f"{REPRO_FIXTURES_ROOT}/multiling2011/data.json", "r")
+        )
+        # self.expected = json.load(open(f"{FIXTURES_ROOT}/expected.json", "r"))
+
+    @parameterized.expand(get_testing_device_parameters())
+    def test_bartscore_examples(self, device: int):
+        # Tests the examples from their Github Repo
+        model = BARTScore(device=device)
+        expected = -2.510652780532837
+        actual = model.predict('This is interesting.', ['This is fun.'])
+        assert actual == pytest.approx(expected, abs=1e-4)
+
+# Test empty input
+# Test switching source target
---
+++ repro/models/yuan2021/setup.py
@@ -0,0 +1,10 @@
+from repro import MODELS_ROOT
+from repro.commands.subcommand import SetupSubcommand
+from repro.common.docker import BuildDockerImageSubcommand
+from repro.models.yuan2021 import DEFAULT_IMAGE, MODEL_NAME
+
+
+@SetupSubcommand.register(MODEL_NAME)
+class Yuan2021SetupSubcommand(BuildDockerImageSubcommand):
+    def __init__(self) -> None:
+        super().__init__(f"{MODELS_ROOT}/{MODEL_NAME}", DEFAULT_IMAGE)
